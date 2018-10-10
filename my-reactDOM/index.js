fe1d000f0d4c1a18dad7fe9a95f9b9aef58abb11
Flora Harvey <fharvey555@gmail.com>
2018-10-10 16:57:41 +0100

    ReactDom.render recursively renders children

--- my-reactDOM/index.js
+++ my-reactDOM/index.js
@@ -1,19 +1,35 @@
+// const element = {
+//   type: 'div',
+//   children: ['hello', 'world'],
+// }
+
 class ReactDOM {
+
   static render(element, container) {
-    let elementToRender;
-
-    // create simple element with text children
-    if (typeof element.type === 'string') {
-      elementToRender = document.createElement(element.type);
-      if (element.children) {
-        elementToRender.innerHTML = element.children.join(' ');
-      }
-    }
-
-    // add the newly created element and its content into the DOM within the
-    // given container
+    const elementToRender = this.createDomElement(element);
     container.appendChild(elementToRender);
   }
+
+  static createDomElement = (element) => {
+    let el;
+    if (typeof element.type === 'string') {
+      el = document.createElement(element.type);
+    }
+    if (element.children) {
+      element.children.forEach(child => {
+        if (typeof child === 'string') {
+          const childElement = document.createTextNode(child);
+          el.appendChild(childElement);
+        } else {
+          const childElement = ReactDOM.createDomElement(child);
+          el.appendChild(childElement);
+        }
+      });
+    }
+    return el;
+  }
+
+
 }
 
 export default ReactDOM;
